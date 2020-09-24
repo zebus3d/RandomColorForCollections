b7f1e9653f8bc1b82f067ca570a58cbcd4042e86
zebus3d <zebus3dream@gmail.com>
2020-09-24 17:50:25 +0200

    con piscina y cycle infinito en orden

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 
 import bpy
 import random
+from bpy.types import (Operator, Panel)
+from itertools import cycle
 
 bl_info = {
     "name": "Collections Color Random",
@@ -12,9 +14,7 @@ bl_info = {
     "warning": "",
     "wiki_url": "",
     "category": "Interface",
-    }
-
-from bpy.types import (Operator, Panel)
+}
 
 
 class RandomColorCollections(Operator):
@@ -33,52 +33,13 @@ class RandomColorCollections(Operator):
         # recreamos un array con los colores hardcoded que trae blende
         # por ahora trae 8 colores
         for i in range(hardcoded_bcolors):
-            colors.append('COLOR_' + str(i+1).zfill(2))
+            colors.append('COLOR_' + str(i + 1).zfill(2))
 
-        # obtenermos un color aleatoreo
-        def get_rand_color():
-            intentos_maximos = hardcoded_bcolors
-            chk_control = 0
-            if len(used_colors) == 0:
-                color = random.choice(colors)
-                return color
-            else:
-                color = random.choice(colors)
-                while color in used_colors and chk_control <= intentos_maximos:
-                    color = random.choice(colors)
-                    chk_control += 1
-                return color
-
-        # obtener un elemento que sea de los menos repetidos de un array:
-        def check_minimun(a):
-            used = []
-            c = 0
-            result = {}
-            for i in a:
-                if i in used:
-                    c += used.count(i)
-                    result[str(i)] = c
-                else:
-                    used.append(i)
-                    c = 1
-                    result[str(i)] = c
-            # return the key corresponding to the minimum value within a dictionary:
-            #print(result)
-            return min(result, key=result.get)
+        pool = cycle(colors)
 
         for col in bpy.data.collections:
-            if hasattr(col, 'color_tag'):
-                color = get_rand_color()
-                # si ya esta siendo usado
-                # entonces buscamos un color que sea de los menos usados
-                if color in used_colors:
-                    color = check_minimun(used_colors)
-                    col.color_tag = color
-                    used_colors.append(color)
-                else:
-                    used_colors.append(color)
-                    col.color_tag = color
-
+            color = next(pool)
+            col.color_tag = color
 
         return {'FINISHED'}
 
@@ -97,18 +58,24 @@ class RC_PT_Collections(Panel):
         flow = layout.grid_flow(align=True)
         col = flow.column()
         # col = layout.box()
-        col.operator("randcol.collections", text="Random Colors for all Collections")
+        col.operator(
+            "randcol.collections",
+            text="Random Colors for all Collections"
+        )
+
 
 all_classes = [
     RandomColorCollections,
     RC_PT_Collections
 ]
 
+
 def register():
     from bpy.utils import register_class
 
     for cls in all_classes:
         register_class(cls)
+
 
 def unregister():
     from bpy.utils import unregister_class
